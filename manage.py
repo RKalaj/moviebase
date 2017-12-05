bb4c611d1ff0afa4717fb15db4b30077eda83cf0
Robert Kalaj <rkalaj@udel.edu>
2017-12-05 13:41:14 -0500

    added database files

--- manage.py
+++ manage.py
@@ -1,7 +1,26 @@
 from flask_script import Manager
-from moviebase import app
+from moviebase import app, db, Director, Movie
 
 manager = Manager(app)
+
+
+@manager.command
+def deploy():
+    db.drop_all()
+    db.create_all()
+    nolan = Director(name='Christopher Nolan', about='Christopher Edward Nolan is an English film director, screenwriter, and producer who holds both British and American citizenship. He is one of the highest-grossing directors in history, and among the most acclaimed filmmakers of the 21st century.')
+    jackson = Director(name='Peter Jackson', about='Sir Peter Robert Jackson is a New Zealand film director, screenwriter and film producer. He is best known as the director, writer, and producer of The Lord of the Rings trilogy (2001-03) and The Hobbit trilogy (2012-14), both of which are adapted from the novels of the same name by J. R. R. Tolkien.')
+    tarantino = Director(name='Quentin Tarantino', about='Quentin Jerome Tarantino is an American film director, writer, and actor. His filmsare characterized by nonlinear storylines, satirical subject matter, an aestheticization of violence, extended scenes of dialogue, ensemble casts consisting of established and lesser-known performers, references to popular culture, soundtracksprimarily containing songs and score pieces from the 1960s to the 1980s, and features of neo-noir film. He is widely considered one of the greatest filmmakers of his generation.')
+    interstellar = Movie(name='Interstellar', year=2014, director=nolan)
+    lotr3 = Movie(name='The Lord of the Rings: The Return of the King', year=2003, director=jackson)
+    inglourious = Movie(name='Inglourious Basterds', year=2009, director=tarantino)
+    db.session.add(nolan)
+    db.session.add(jackson)
+    db.session.add(tarantino)
+    db.session.add(interstellar)
+    db.session.add(lotr3)
+    db.session.add(inglourious)
+    db.session.commit()
 
 
 if __name__=='__main__':
--- moviebase.py
+++ moviebase.py
@@ -1,5 +1,32 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, session, request, flash, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'our secret key'
+
+# setup SQLAlchemy
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+db = SQLAlchemy(app)
+
+
+# define database tables
+class Director(db.Model):
+    __tablename__ = 'director'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    about = db.Column(db.Text)
+    movie = db.relationship('Movie', backref='director', cascade="delete")
+
+
+class Movie(db.Model):
+    __tablename__ = 'movie'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(256))
+    year = db.Column(db.Integer)
+    director_id = db.Column(db.Integer, db.ForeignKey('director.id'))
+
 
 @app.route('/')
 def home():
@@ -11,6 +38,13 @@ def home():
 def members():
     # return '<h1>hello world!</h1>'
     return render_template('members.html')
+
+
+@app.route('/directors')
+def show_all_directors():
+    director = Director.query.all()
+    return render_template('director-all.html', director=director)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
